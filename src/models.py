9b1856a31457ba8d2c5d2bda8c80a60682707e55
JerryJox <profesionalmail@msn.com>
2023-05-17 23:42:36 +0200

    upload files

--- src/models.py
+++ src/models.py
@@ -7,23 +7,90 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class Films(Base):
+    __tablename__ = 'films'
+    id_films = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    characters = Column(Integer, ForeignKey('character.id_character'))
+    starships = Column(Integer, ForeignKey('starship.id_starship'))
+    planets = Column(Integer, ForeignKey('planet.id_planet'))
+
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    id_character = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    bith_year = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    homeworld_id = Column(Integer, ForeignKey('planet.id_planet'))
+    mass = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    species = Column(String(250), nullable=False)
+    starship = Column(Integer, ForeignKey('starship.id_starship'))
+    films = Column(Integer, ForeignKey('film.id_film'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Starship(Base):
+    __tablename__ = 'starship'
+    id_starship = Column(Integer, primary_key=True)
+    cargo_capacity = Column(String(250), nullable=False)
+    consumable = Column(String(250), nullable=False)
+    cost_in_credits = Column(String(250), nullable=False)
+    crew = Column(String(250), nullable=False)
+    hyperdrive_rating = Column(String(250), nullable=False)
+    MGLT = Column(String(250), nullable=False)
+    length = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    max_atmosfering_speed = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    passengers = Column(String(250), nullable=False)
+    films = Column(Integer, ForeignKey('film.id_film'))
+    pilots = relationship(Character)
+    starship_class = Column(String(250), nullable=False)
+
+
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id_planet = Column(Integer, primary_key=True)
+    climate = Column(String(250), nullable=False)
+    films = Column(Integer, ForeignKey('film.id_film'))
+    gravity = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    orbital_period = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    residents = Column(String(250), nullable=False)
+    rotation_period = Column(String(250), nullable=False)
+    surface_water = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id_user = Column(Integer, primary_key=True)  
+    id_character = Column(Integer, ForeignKey('character.id_character'))
+    id_starship = Column(Integer, ForeignKey('starship.id_starship'))
+    id_planet = Column(Integer, ForeignKey('planet.id_planet'))
+    id_films = Column(Integer, ForeignKey('film.id_film'))
+
+class User(Base):
+    __tablename__ = 'user'
+    id_user = Column(Integer, primary_key=True)  
+    email = Column(Integer, primary_key=True)
+    password = Column(Integer, primary_key=True)
+    
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Character)
 
     def to_dict(self):
         return {}
